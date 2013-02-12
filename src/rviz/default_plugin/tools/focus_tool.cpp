2cae380974288ff94f9a997891ee2c779af84927
David Gossow <dgossow@willowgarage.com>
2013-02-12 14:01:58 -0800

    Rebase from 'release/rviz' @ version '1.9.21'

--- src/rviz/default_plugin/tools/focus_tool.cpp
+++ src/rviz/default_plugin/tools/focus_tool.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Willow Garage, Inc.
+ * Copyright (c) 2013, Willow Garage, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,117 +26,86 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
- /*
- * measure_tool.cpp
- *
- *  Created on: Aug 8, 2012
- *      Author: gossow
- */
 
-#include "measure_tool.h"
+#include <OGRE/OgreRay.h>
+#include <OGRE/OgreVector3.h>
 
-#include "rviz/ogre_helpers/line.h"
 #include "rviz/viewport_mouse_event.h"
+#include "rviz/load_resource.h"
+#include "rviz/render_panel.h"
 #include "rviz/display_context.h"
 #include "rviz/selection/selection_manager.h"
-#include "rviz/load_resource.h"
+#include "rviz/view_controller.h"
 
-#include <OgreSceneNode.h>
+#include "rviz/default_plugin/tools/focus_tool.h"
 
 #include <sstream>
 
 namespace rviz
 {
 
-MeasureTool::MeasureTool() :
-    state_(START),
-    length_(-1)
+FocusTool::FocusTool()
+  : Tool()
 {
 }
 
-MeasureTool::~MeasureTool()
+FocusTool::~FocusTool()
 {
-  delete line_;
 }
 
-void MeasureTool::onInitialize()
+void FocusTool::onInitialize()
 {
-  line_ = new Line(context_->getSceneManager());
-
   std_cursor_ = getDefaultCursor();
   hit_cursor_ = makeIconCursor( "package://rviz/icons/crosshair.svg" );
 }
 
-void MeasureTool::activate()
-{
-  state_ = START;
-}
-
-void MeasureTool::deactivate()
+void FocusTool::activate()
 {
 }
 
-int MeasureTool::processMouseEvent( ViewportMouseEvent& event )
+void FocusTool::deactivate()
+{
+}
+
+int FocusTool::processMouseEvent( ViewportMouseEvent& event )
 {
   int flags = 0;
 
   Ogre::Vector3 pos;
-
-  std::stringstream ss;
-
   bool success = context_->getSelectionManager()->get3DPoint( event.viewport, event.x, event.y, pos );
   setCursor( success ? hit_cursor_ : std_cursor_ );
 
-  switch ( state_ )
+  if ( !success )
   {
-    case START:
-      break;
-    case END:
-      if ( success )
-      {
-        line_->setPoints(start_,pos);
-        length_ = (start_-pos).length();
-      }
-      break;
+    Ogre::Ray mouse_ray = event.viewport->getCamera()->getCameraToViewportRay(
+        (float)event.x / (float)event.viewport->getActualWidth(),
+        (float)event.y / (float)event.viewport->getActualHeight() );
+
+    pos = mouse_ray.getPoint(1.0);
+    setStatus( "<b>Left-Click:</b> Look in this direction." );
+  }
+  else
+  {
+    std::ostringstream s;
+    s << "<b>Left-Click:</b> Focus on this point.";
+    s.precision(3);
+    s << " [" << pos.x << "," << pos.y << "," << pos.z << "]";
+    setStatus( s.str().c_str() );
   }
 
-  if ( length_ > 0.0 )
+  if( event.leftUp() )
   {
-    ss << "[Length: " << length_ << "m] ";
-  }
-
-  ss << "Click on two points to measure their distance. Right-click to reset.";
-  setStatus( QString( ss.str().c_str() ) );
-
-  if( event.leftUp() && success )
-  {
-    switch ( state_ )
+    if ( event.panel->getViewController() )
     {
-      case START:
-        start_ = pos;
-        state_ = END;
-        break;
-      case END:
-        end_ = pos;
-        state_ = START;
-        line_->setPoints(start_,end_);
-        break;
+      event.panel->getViewController()->lookAt( pos );
     }
-
-    flags |= Render;
-  }
-
-  if ( event.rightUp() )
-  {
-    state_ = START;
-    line_->setVisible(false);
+    flags |= Finished;
   }
 
   return flags;
 }
 
-} /* namespace rviz */
-
+}
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS( rviz::MeasureTool, rviz::Tool )
+PLUGINLIB_EXPORT_CLASS( rviz::FocusTool, rviz::Tool )
