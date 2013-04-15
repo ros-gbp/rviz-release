8db708dcd482d4b1bcd9c7b8674a0cff5811bd4d
David Gossow <dgossow@willowgarage.com>
2013-04-15 16:21:12 -0700

    Rebase from 'upstream'

--- src/rviz/ogre_helpers/render_system.cpp
+++ src/rviz/ogre_helpers/render_system.cpp
@@ -53,10 +53,13 @@
 
 #include "rviz/ogre_helpers/render_system.h"
 
+#include <QMessageBox>
+
 namespace rviz
 {
 
 RenderSystem* RenderSystem::instance_ = 0;
+int RenderSystem::force_gl_version_ = 0;
 
 RenderSystem* RenderSystem::get()
 {
@@ -65,6 +68,12 @@ RenderSystem* RenderSystem::get()
     instance_ = new RenderSystem();
   }
   return instance_;
+}
+
+void RenderSystem::forceGlVersion( int version )
+{
+  force_gl_version_ = version;
+  ROS_INFO_STREAM( "Forcing OpenGl version " << (float)version / 100.0 << "." );
 }
 
 RenderSystem::RenderSystem()
@@ -122,12 +131,20 @@ void RenderSystem::loadOgrePlugins()
 
 void RenderSystem::detectGlVersion()
 {
-  Ogre::RenderSystem *renderSys = ogre_root_->getRenderSystem();
-  renderSys->createRenderSystemCapabilities();
-  const Ogre::RenderSystemCapabilities* caps = renderSys->getCapabilities();
-  int major = caps->getDriverVersion().major;
-  int minor = caps->getDriverVersion().minor;
-  gl_version_ = major * 100 + minor*10;
+  if ( force_gl_version_ )
+  {
+    gl_version_ = force_gl_version_;
+  }
+  else
+  {
+    Ogre::RenderSystem *renderSys = ogre_root_->getRenderSystem();
+    renderSys->createRenderSystemCapabilities();
+    const Ogre::RenderSystemCapabilities* caps = renderSys->getCapabilities();
+    int major = caps->getDriverVersion().major;
+    int minor = caps->getDriverVersion().minor;
+    gl_version_ = major * 100 + minor*10;
+  }
+
   switch ( gl_version_ )
   {
     case 200:
@@ -156,7 +173,7 @@ void RenderSystem::detectGlVersion()
       }
       break;
   }
-  ROS_INFO_STREAM( "OpenGl version: " << major << "." << minor << " (GLSL " << (float)glsl_version_ / 100.0 << ")." );
+  ROS_INFO_STREAM( "OpenGl version: " << (float)gl_version_ / 100.0 << " (GLSL " << (float)glsl_version_ / 100.0 << ")." );
 }
 
 void RenderSystem::setupRenderSystem()
@@ -213,6 +230,7 @@ void RenderSystem::setupResources()
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials", "FileSystem", ROS_PACKAGE_NAME );
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/scripts", "FileSystem", ROS_PACKAGE_NAME );
   Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/glsl120", "FileSystem", ROS_PACKAGE_NAME );
+  Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/glsl120/nogp", "FileSystem", ROS_PACKAGE_NAME );
   // Add resources that depend on a specific glsl version.
   // Unfortunately, Ogre doesn't have a notion of glsl versions so we can't go
   // the 'official' way of defining multiple schemes per material and let Ogre decide which one to use.
@@ -221,9 +239,17 @@ void RenderSystem::setupResources()
     Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/glsl150", "FileSystem", ROS_PACKAGE_NAME );
     Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/scripts150", "FileSystem", ROS_PACKAGE_NAME );
   }
-  else
+  else if ( getGlslVersion() >= 120  )
   {
     Ogre::ResourceGroupManager::getSingleton().addResourceLocation( rviz_path + "/ogre_media/materials/scripts120", "FileSystem", ROS_PACKAGE_NAME );
+  }
+  else
+  {
+    std::string s = "Your graphics driver does not support OpenGL 2.1. Please enable software rendering before running RViz (e.g. type 'export LIBGL_ALWAYS_SOFTWARE=1').";
+    QMessageBox msgBox;
+    msgBox.setText(s.c_str());
+    msgBox.exec();
+    throw std::runtime_error( s );
   }
 }
 
