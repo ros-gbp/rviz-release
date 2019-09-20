6364959b872110bbdaee8c3e1c04ae1e0e8a09b2
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-09-20 15:42:24 +0200

    Rebase from 'debian/melodic/rviz'

--- src/rviz/default_plugin/marker_utils.h
+++ src/rviz/default_plugin/marker_utils.h
@@ -30,6 +30,10 @@
 #ifndef RVIZ_MARKER_UTILS_H
 #define RVIZ_MARKER_UTILS_H
 
+
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
 namespace Ogre
 {
 class SceneNode;
@@ -44,6 +48,11 @@ class MarkerBase;
 
 /** Create a marker of given type as declared in visualization_messages::Marker */
 MarkerBase* createMarker(int marker_type, MarkerDisplay *owner, DisplayContext *context, Ogre::SceneNode *parent_node);
+
+/** Check for correctness of the marker description, issue a warning/error status in the owner if not
+ *  Return false if there is a severe error meaning that this marker should be dropped */
+bool checkMarkerMsg(const visualization_msgs::Marker& marker, MarkerDisplay* owner);
+bool checkMarkerArrayMsg(const visualization_msgs::MarkerArray& array, MarkerDisplay* owner);
 
 }
 
